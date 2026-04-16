89873964edc663e486baa5c49d5d95533c61ac24
renpython36 <ren.aba36@gmail.com>
2026-04-16 18:50:11 +0100

    OK

---
+++ lesson 2/activity 1 copy.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+
+_Name = input("WHat is your Name?")
+
+
+print("So your'e is ", _Name)
+
+
+print("Hello everyone")
+
+_Age = input("What is your age?")
+
+
+print("So your age is", _Age)
---
+++ lesson 2/activity 1.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+
+MyNumber  =   7006707488
+
+
+MyNewNumber = MyNumber +10
+
+print(MyNewNumber)
+
+
+
+
+print("Welcome everyone")
+
+
+MyName = "Renee" 
+
+
+print(MyName)
+
+
+print("Hello Y'all, My Name Is ", MyName)
